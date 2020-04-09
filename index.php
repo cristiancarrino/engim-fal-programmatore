dd0d758f5bdb4eb6a011fdff9712ae4d5ba1ffff
Cristian Carrino <cristian.carrino@libero.it>
2020-04-09 11:16:55 +0200

    Lezione php 1

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Engim - Homepage</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>    
+    <nav class="navbar">
+        <h1>Dashboard</h1>
+        <div class="menu">
+            <a href="" class="navbar-link">Lista studenti</a>
+            <a href="" class="navbar-link">Lista professori</a>
+            <a href="" class="navbar-link">Lista materie</a>
+        </div>
+        <div class="clearfix"></div>
+    </nav>
+
+    <div class="footer">
+        Credits: Cristian Carrino
+    </div>
+</body>
+</html>
