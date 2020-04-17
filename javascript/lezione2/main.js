76bd78c9d353390c83603754a1f4d85a3b5fe98d
Cristian Carrino <cristian.carrino@libero.it>
2020-04-17 12:36:28 +0200

    Continuo con la lezione 2

--- javascript/lezione2/main.js
+++ javascript/lezione2/main.js
@@ -15,3 +15,41 @@ function changeColorToH2 () {
         elements[i].style.color = color;
     }
 }
+
+function changeColorToH3 () {
+    var select = document.getElementById('selected-color');
+    var color = select.value;
+
+    var elements = document.getElementsByTagName('h3');
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].style.color = color;
+    }
+}
+
+function addBorderedToH3 () {
+    var elements = document.getElementsByTagName('h3');
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].classList.add('bordered');
+    }
+}
+
+function removeBorderedToH3 () {
+    var elements = document.getElementsByTagName('h3');
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].classList.remove('bordered');
+    }
+}
+
+function addRedToOrnella () {
+    var elements = document.getElementsByClassName('ornella');
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].classList.add('red');
+    }
+}
+
+function removeRedToOrnella () {
+    var elements = document.getElementsByClassName('ornella');
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].classList.remove('red');
+    }
+}
